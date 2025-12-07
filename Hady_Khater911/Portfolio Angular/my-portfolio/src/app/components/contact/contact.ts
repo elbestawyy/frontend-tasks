76f2006afb1105932a961d608c551202b2c9753e
Hady Khater <hadyhanykhater@gmail.com>
2025-12-07 11:41:35 +0200

    Hady

--- Hady_Khater911/Portfolio Angular/my-portfolio/src/app/components/contact/contact.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-contact',
-  standalone: true,
-  templateUrl: './contact.html',
-  styleUrl: './contact.css',
-})
-export class Contact {}
